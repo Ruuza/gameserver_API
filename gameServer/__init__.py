68214b8e49d97324792cb80f605d0c7d1c2b4226
Ruuza <RunyCZ@gmail.com>
2021-03-23 10:50:22 +0100

    use pymysql

--- gameServer/__init__.py
+++ gameServer/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from gameServer.config import Config
+import pymysql
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config.from_object(Config)
