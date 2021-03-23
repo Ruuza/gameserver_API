bde2e6b165c47dd5c6996eb4fbb40db2354e576b
Ruuza <RunyCZ@gmail.com>
2021-03-23 23:44:08 +0100

    updated config.py

--- gameServer/config.py
+++ gameServer/config.py
@@ -2,4 +2,4 @@
 
 class Config:
     SECRET_KEY = '12d5807fa890e39025'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:raj.ce258@localhost/game_server'
+    SQLALCHEMY_DATABASE_URI = 'mysql://user:password@localhost/db_name'
